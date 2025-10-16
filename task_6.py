0c4a59d979a072b2d4982bb319f80422d85a04f1
ModeLoader <modeloader@mail.ru>
2025-10-16 14:10:59 +0000

    hhkh

---
+++ task_6.py
@@ -0,0 +1,13 @@
+age = input('Введите свой возраст :' )
+gender = input('Введите свой пол :' )
+name = input('Введите своё имя :' )
+town = input('Введите свой город :' )
+klass = input('Введите номер своего класса :' )
+
+spisok = []
+spisok.append(age)
+spisok.append(gender)
+spisok.append(name)
+spisok.append(town)
+spisok.append(klass)
+print(spisok)
