1ece7be416926c51c23ec5f2d02b6d1428019886
ModeLoader <modeloader@mail.ru>
2025-11-06 15:43:07 +0000

    commit

--- task_2.1.py
+++ task_2.1.py
@@ -2,8 +2,13 @@ Otr1 = int(input("Введите 1-ую длину отрезка: "))
 Otr2 = int(input("Введите 2-ую длину отрезка: "))
 Otr3 = int(input("Введите 3-ую длину отрезка: "))
 
-if (Otr1 < Otr2+Otr3) and (Otr2 < Otr1+Otr3) and (Otr3< Otr2+Otr1):
+if (Otr1 <= Otr2+Otr3) and (Otr2 <= Otr1+Otr3) and (Otr3 <= Otr2+Otr1):
     print(f"Треугольник со сторонами {Otr1},{Otr2} и {Otr3} существует")
-    if 
+    if Otr1 != Otr2 != Otr3:
+        print("Данный треугольник является разносторонним")
+    elif Otr1 == Otr2 == Otr3 :
+        print("Данный треугольник является равносторонним")
+    else:
+        print("Данный треугольник является равнобедренным")
 else:
     print(f"Треугольник со сторонами {Otr1},{Otr2} и {Otr3}  не существует")
---
+++ task_3.1.py
@@ -0,0 +1,5 @@
+chislo = input()
+long = list(chislo)
+for i in range(1 , len(long)):
+    n = chislo % 10
+    print(len(chislo))
